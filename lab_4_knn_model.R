da5eaa25dce4b1e034f17db0f2c3b3775c5e4052
Jim Wright <jjwright90@gmail.com>
2020-11-05 16:59:26 -0800

    Question 2 and 3 updates

--- lab_4_knn_model.R
+++ lab_4_knn_model.R
@@ -15,7 +15,7 @@ clusterEvalQ(cl, {library(tidymodels)})
 
 full_train <- read_csv("data/train.csv")
 
-data_sample <- dplyr::sample_frac(full_train, size = 0.005)
+#data_sample <- dplyr::sample_frac(full_train, size = 0.005)
 
 # create initial splits 
 set.seed(3000)
@@ -35,5 +35,32 @@ class(test_split)
 set.seed(3000)
 
 cv_splits <- vfold_cv(train_split, v = 10)
+
+
+# basic recipe
+knn01_rec <- 
+  recipe(classification ~ enrl_grd + lat + lon + gndr, data = train_split) %>%
+  step_mutate(classification = ifelse (classification < 3, "below", "proficient")) %>%
+  step_mutate(enrl_grd = factor(enrl_grd), gndr = factor(gndr)) %>%
+  step_meanimpute(lat, lon) %>%
+  step_unknown(enrl_grd, gndr) %>% 
+  step_dummy(enrl_grd, gndr) %>% 
+  step_normalize(lat, lon)
+
+# set model
+knn01_mod <- 
+  nearest_neighbor() %>% 
+  set_engine("kknn") %>% 
+  set_mode("classification")
+
+translate(knn01_mod)
+
+#install.packages("kknn")
+library(kknn)
+
+fit1 <- fit_resamples(knn01_mod, knn01_rec, cv_splits)
+saveRDS(fit1, "fit1.Rds")
+
+
 
 
