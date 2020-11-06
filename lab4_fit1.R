e7ec0080055a4fdcac60418cac99f8b87ad08395
claire zhang <claireconglizhang@gmail.com>
2020-11-06 01:51:58 -0800

    add fit2 for q4

--- lab4_fit1.R
+++ lab4_fit1.R
@@ -1,11 +1,11 @@
 source("lab4_common.R")
 
 # set model
-knn01_mod <- 
+knn1_mod <- 
   nearest_neighbor() %>% 
   set_engine("kknn") %>% 
   set_mode("classification")
 
-fit1 <- fit_resamples(knn01_mod, rec, cv_splits)
+fit1 <- fit_resamples(knn1_mod, rec, cv_splits)
 
 saveRDS(fit1, "lab4_fit1.Rds")
---
+++ lab4_fit2.R
@@ -0,0 +1,27 @@
+source("lab4_common.R")
+
+# set model
+knn2_mod <- 
+  nearest_neighbor() %>% 
+  set_engine("kknn") %>% 
+  set_mode("classification") %>% 
+  set_args(neighbors = tune(),
+           weight_func = tune(),
+           dist_power = tune())
+
+# make a non-regular grid
+
+knn_params <- parameters(neighbors(range = c(1, 20)),
+                         weight_func(),
+                         dist_power())
+
+knn_sfd <- grid_max_entropy(knn_params, size = 25)
+
+fit2 <- tune::tune_grid(
+  knn2_mod, 
+  rec, 
+  cv_splits,
+  grid = knn_sfd,
+  control = tune::control_resamples(save_pred = TRUE))
+
+saveRDS(fit2, "lab4_fit2.Rds")
